939f004cb0f41814ca529f47ea19071f606d1602
zavala <angel@example.com>
2025-03-13 18:12:24 -0600

    Modificacion de los Archivos de Modelos

--- app/Depuracion.php
+++ app/Depuracion.php
@@ -6,19 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Depuracion extends Model
 {
-    protected $table = 'bodega.sol_depuraciones';
+  protected $table = 'bodega.listdepuraciones';
 
-    protected $fillable = [
-      'nuc',
-      'fecha_solicitud',
-      'M.P_solicitud',
-      'unidad_solicitud',
-      'fecha_recepcion_solicitud',
+  protected $fillable = [
+      'fecha',
+      'hora',
+      'depuracion_numindicios',
       'observaciones',
+      'soldepuracion',
+      'cadena_id',
       'user_id',//User responsable de la creacion del registro
     ];
 
+    public function indicios(){
+      return $this->belongsToMany('App\Indicio','indicio_listdepuracion','indicio_id','listdepuracion_id')
+                  ->withPivot('id','listdepuracion_tipo')
+                  ->withTimestamps();
+   }
+
+    public function solicitud(){
+      return $this->belongsTo('App\Depuracion');
+    }
+
+    public function cadena(){
+      return  $this->belongsTo('App\Cadena');
+    }
+
     public function user(){
-        return $this->belongsTo('App\User');
-      }
+      return $this->belongsTo('App\User');
+    }
 }
---
+++ app/Soltdepuracion.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Soltdepuracion extends Model
+{
+    protected $table = 'bodega.sol_depuraciones';
+
+    protected $fillable = [
+      'nuc',
+      'fecha_solicitud',
+      'M.P_solicitud',
+      'unidad_solicitud',
+      'fecha_recepcion_solicitud',
+      'observaciones',
+      'user_id',//User responsable de la creacion del registro
+    ];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+      }
+}
